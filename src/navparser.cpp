b61e72d85eabc5ec1f998f6616b6cc0842472d3c
MasterCatPL <robercik9999999@gmail.com>
2024-01-14 19:59:37 +0100

    changed files

--- src/navparser.cpp
+++ src/navparser.cpp
@@ -963,13 +963,11 @@ void Draw()
         Vector start_screen, end_screen;
         if (draw::WorldToScreen(start_pos, start_screen))
         {
-            draw::Rectangle(start_screen.x - 5.0f, start_screen.y - 5.0f, 10.0f, 10.0f, colors::white);
-
             if (i < crumbs.size() - 1)
             {
                 Vector end_pos = crumbs[i + 1].vec;
                 if (draw::WorldToScreen(end_pos, end_screen))
-                    draw::Line(start_screen.x, start_screen.y, end_screen.x - start_screen.x, end_screen.y - start_screen.y, colors::white, 2.0f);
+                    draw::Line(start_screen.x, start_screen.y, end_screen.x - start_screen.x, end_screen.y - start_screen.y, colors::green, 2.0f);
             }
         }
     }
--- src/votelogger.cpp
+++ src/votelogger.cpp
@@ -136,7 +136,7 @@ void dispatchUserMessage(bf_read &buffer, int type)
         if (*chat_partysay)
         {
             char formated_string[256];
-            std::snprintf(formated_string, sizeof(formated_string), "[CAT] [%s] votekick called: %s => %s (%s)", team_name, info2.name, info.name, reason);
+            std::snprintf(formated_string, sizeof(formated_string), "[PL] [%s] votekick called: %s => %s (%s)", team_name, info2.name, info.name, reason);
             re::CTFPartyClient::GTFPartyClient()->SendPartyChat(formated_string);
         }
 #if ENABLE_VISUALS
