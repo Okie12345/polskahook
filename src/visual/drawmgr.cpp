dfc29f5a7d6b03a25d67113563cbd536ab35a979
MasterCatPL <robercik9999999@gmail.com>
2024-01-22 17:20:12 +0100

    E

--- src/visual/drawmgr.cpp
+++ src/visual/drawmgr.cpp
@@ -21,15 +21,10 @@
 #include <menu/GuiInterface.hpp>
 #include "common.hpp"
 #include "visual/drawing.hpp"
-#include "menu/menu/Menu.hpp"
+#include "hack.hpp"
 #include "drawmgr.hpp"
 
 static settings::Boolean info_text{ "hack-info.enable", "true" };
-static settings::Int info_style{ "hack-info.style", "0" };
-static settings::Rgba info_background_color{"hack-info.background", "00000b3"};
-static settings::Rgba info_foreground_color{"hack-info.foreground", "ffffff"};
-static settings::Int info_x{"hack-info.x", "10"};
-static settings::Int info_y{"hack-info.y", "10"};
 
 void RenderCheatVisuals()
 {
@@ -64,46 +59,36 @@ void BeginCheatVisuals()
     ResetStrings();
 }
 
-double getRandom(double lower_bound, double upper_bound)
-{
-    std::uniform_real_distribution<double> unif(lower_bound, upper_bound);
-    static std::mt19937 rand_engine(std::time(nullptr));
-
-    double x = unif(rand_engine);
-    return x;
-}
-
 void DrawCheatVisuals()
 {
     {
+        PROF_SECTION(DRAW_info)
         std::string name_s, reason_s;
-        if (info_text)
+        if (*info_text && (!g_IEngine->IsConnected() || g_IEngine->Con_IsVisible()))
         {
-            float w, h;
-            std::string hack_info_text;
-            if (*info_style == 0) {
-                hack_info_text = "PolskaHook";
-                fonts::center_screen->stringSize(hack_info_text, &w, &h);
-                draw::String(*info_x, *info_y, *info_foreground_color, hack_info_text.c_str(), *fonts::center_screen);
-            }
+            auto color = colors::white;
+            AddSideString("PolskaHook", color);
         }
     }
     if (spectator_target)
-    {
         AddCenterString("Press SPACE to stop spectating");
-    }
     {
+        PROF_SECTION(DRAW_WRAPPER)
         EC::run(EC::Draw);
     }
-    if (CE_GOOD(g_pLocalPlayer->entity) && !g_Settings.bInvalid)
+    if (CE_GOOD(LOCAL_E))
     {
+        PROF_SECTION(DRAW_skinchanger)
+        hacks::skinchanger::DrawText();
         Prediction_PaintTraverse();
     }
     {
+        PROF_SECTION(DRAW_strings)
         DrawStrings();
     }
 #if ENABLE_GUI
     {
+        PROF_SECTION(DRAW_GUI)
         gui::draw();
     }
 #endif
