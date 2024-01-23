46de0772f4bcb84132f73978cbda473962979871
MasterCatPL <robercik9999999@gmail.com>
2024-01-23 16:53:26 +0100

    added rosnehook to Spam

--- include/hacks/Spam.hpp
+++ include/hacks/Spam.hpp
@@ -18,6 +18,7 @@ extern const std::vector<std::string> builtin_lennyfaces;
 extern const std::vector<std::string> builtin_nonecore;
 extern const std::vector<std::string> builtin_lmaobox;
 extern const std::vector<std::string> builtin_cowhook;
+extern const std::vector<std::string> builtin_rosnehook;
 
 bool isActive();
 void init();
