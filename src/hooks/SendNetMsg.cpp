d79a4517b79db86f1e54481f462b35fddc2593b8
Rosne <68449858+rosne-gamingyt@users.noreply.github.com>
2024-01-28 23:54:50 -0500

    remove nullnexus and identify (broken and useless)

--- src/hooks/SendNetMsg.cpp
+++ src/hooks/SendNetMsg.cpp
@@ -7,18 +7,11 @@
 #include <settings/Int.hpp>
 #include "AntiAim.hpp"
 #include "HookedMethods.hpp"
-#include "nullnexus.hpp"
 #include "Warp.hpp"
 #include "nospread.hpp"
 #include "AntiCheatBypass.hpp"
 
-static settings::Int newlines_msg{ "chat.prefix-newlines", "0" };
 static settings::Boolean log_sent{ "debug.log-sent-chat", "false" };
-static settings::Boolean answerIdentify{ "chat.identify.answer", "true" };
-static Timer identify_timer{};
-constexpr int CAT_IDENTIFY   = 0xCA7;
-constexpr int CAT_REPLY      = 0xCA8;
-constexpr float AUTH_MESSAGE = 1234567.0f;
 
 namespace hacks::catbot
 {
@@ -26,25 +19,6 @@ void SendNetMsg(INetMessage &msg);
 }
 namespace hooked_methods
 {
-
-static bool send_achievement_reply{};
-static Timer send_achievement_reply_timer{};
-
-// Welcome back Achievement based identify.
-void sendAchievementKv(int value)
-{
-    auto *kv = new KeyValues("AchievementEarned");
-    kv->SetInt("achievementID", value);
-    g_IEngine->ServerCmdKeyValues(kv);
-}
-
-void sendIdentifyMessage(bool reply)
-{
-    reply ? sendAchievementKv(CAT_REPLY) : sendAchievementKv(CAT_IDENTIFY);
-}
-
-settings::Boolean identify{ "chat.identify", "true" };
-
 std::vector<KeyValues *> Iterate(KeyValues *event, int depth)
 {
     std::vector<KeyValues *> peer_list = { event };
@@ -126,71 +100,6 @@ void ParseKeyValue(KeyValues *event)
     }
 }
 
-void ProcessAchievement(IGameEvent *ach)
-{
-    int player_idx  = ach->GetInt("player", 0xDEAD);
-    int achievement = ach->GetInt("achievement", 0xDEAD);
-    if (player_idx != 0xDEAD && (achievement == CAT_IDENTIFY || achievement == CAT_REPLY))
-    {
-        // Always reply and set on CA7 and only set on CA8
-        bool reply = achievement == CAT_IDENTIFY;
-        player_info_s info{};
-        if (!g_IEngine->GetPlayerInfo(player_idx, &info))
-        {
-            return;
-        }
-
-        if (reply && *answerIdentify && player_idx != g_pLocalPlayer->entity_idx)
-        {
-            send_achievement_reply_timer.update();
-            send_achievement_reply = true;
-        }
-
-        if (playerlist::ChangeState(info.friendsID, playerlist::k_EState::CAT))
-        {
-            PrintChat("Detected \x07%06X%s\x01 as a Cathook user", 0xe1ad01, info.name);
-        }
-    }
-}
-
-class AchievementListener : public IGameEventListener2
-{
-    void FireGameEvent(IGameEvent *event) override
-    {
-        ProcessAchievement(event);
-    }
-};
-static CatCommand send_identify("debug_send_identify", "debug", []() { sendIdentifyMessage(false); });
-
-static AchievementListener event_listener{};
-
-static InitRoutine run_identify(
-    []()
-    {
-        EC::Register(
-            EC::CreateMove,
-            []()
-            {
-                if (send_achievement_reply && send_achievement_reply_timer.check(10000))
-                {
-                    sendIdentifyMessage(true);
-                    send_achievement_reply = false;
-                }
-
-                // It is safe to send every 15ish seconds, small packet
-                if (!*identify || CE_BAD(LOCAL_E) || !identify_timer.test_and_set(15000))
-                {
-                    return;
-                }
-
-                sendIdentifyMessage(false);
-            },
-            "sendnetmsg_createmove");
-        g_IEventManager2->AddListener(&event_listener, "achievement_earned", false);
-        EC::Register(
-            EC::Shutdown, []() { g_IEventManager2->RemoveListener(&event_listener); }, "shutdown_event");
-    });
-
 DEFINE_HOOKED_METHOD(SendNetMsg, bool, INetChannel *this_, INetMessage &msg, bool force_reliable, bool voice)
 {
     if (!isHackActive())
@@ -215,52 +124,6 @@ DEFINE_HOOKED_METHOD(SendNetMsg, bool, INetChannel *this_, INetMessage &msg, boo
     }
 
     hacks::antianticheat::SendNetMsg(msg);
-
-    // net_StringCmd
-    if (msg.GetType() == 4 && (newlines_msg || crypt_chat))
-    {
-        std::string str(msg.ToString());
-        say_idx      = str.find("net_StringCmd: \"say \"");
-        say_team_idx = str.find("net_StringCmd: \"say_team \"");
-        if (!say_idx || !say_team_idx)
-        {
-            offset    = say_idx ? 26 : 21;
-
-#if ENABLE_NULLNEXUS
-            // Only allow !! and !!! if crypto_chat is on
-            if (crypt_chat)
-            {
-                std::string msg(str.substr(offset));
-                msg = msg.substr(0, msg.length() - 2);
-                if (msg.find("!!!") == 0 || msg.find("!!") == 0)
-                {
-                    int sub_val = 2;
-                    if (msg.find("!!!") == 0)
-                    {
-                        sub_val = 3;
-                    }
-
-                    // Message is sent over Nullnexus.
-                    std::string substrmsg(msg.substr(sub_val));
-                    nullnexus::sendmsg(substrmsg);
-                    // Do not send message over normal chat.
-                    return false;
-                }
-            }
-#endif
-            if (*newlines_msg > 0)
-            {
-                // TODO move out? update in a value change callback?
-                newlines = std::string(*newlines_msg, '\n');
-                str.insert(offset, newlines);
-            }
-            str = str.substr(16, str.length() - 17);
-            // if (queue_messages && !chat_stack::CanSend()) {
-            stringcmd.m_szCommand = str.c_str();
-            return original::SendNetMsg(this_, stringcmd, force_reliable, voice);
-            //}
-        }
-    }
     static float lastcmd = 0.0f;
     if (lastcmd > g_GlobalVars->absoluteframetime)
     {
